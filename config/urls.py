0df5761b27a72f4c0c615ad39c4e4e114644499f
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-06-18 08:57:56 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.http import JsonResponse
 
 def health_check(request):
@@ -8,5 +9,8 @@ def health_check(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tickets.urls')),
-    path('api/healthz', health_check),
+    #path('api/healthz', health_check),
+    
+    # Redirigir la raíz de la página directamente a /api/
+    path('', RedirectView.as_view(url='api/', permanent=False)),
 ]

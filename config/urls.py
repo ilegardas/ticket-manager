856f49d344f15e8291b9e3c64d961443cdf467fe
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-06-18 09:15:24 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.http import JsonResponse
-
-def health_check(request):
-    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tickets.urls')),
-    #path('api/healthz', health_check),
+    path('api/', include('tickets.urls')), # O la ruta de tus endpoints
     
-    # Redirigir la raíz de la página directamente a /api/
-    path('', RedirectView.as_view(url='api/', permanent=False)),
+    # Esto obliga a que la raíz redirija automáticamente a la sección de la API
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]

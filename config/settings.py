7a3c18e747f6a8a38e3da09d75e69da6d2a6d56c
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-06-18 09:01:12 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
     }
 else:
     DATABASES = {

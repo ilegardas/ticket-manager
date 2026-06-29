9b227a06576795c3651635dafc1251719410bfb0
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-06-29 11:41:01 -0600

    Update views.py

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import authenticate
 from django.utils import timezone

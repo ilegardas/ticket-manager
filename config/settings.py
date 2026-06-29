22ab46f4fa86aa4b1dfb3605a80171ae5bedbc14
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-06-29 09:48:59 -0600

    Add global templates directory to TEMPLATES setting
    Actualizacion para vistas internas

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 🎯 Añadimos la ruta hacia la carpeta global de plantillas
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

5e278d65c570b7d538890bcfb74e175cfa2baa90
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-07-01 11:27:32 -0600

    Add threading import to views.py

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+import threading
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse

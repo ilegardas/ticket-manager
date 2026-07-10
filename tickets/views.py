95acda3d510ad61a5b99be9f1bc27bd59617ffe5
ilegardas <44457061+ilegardas@users.noreply.github.com>
2026-07-10 11:33:56 -0600

    Add 'date' import to views.py

--- tickets/views.py
+++ tickets/views.py
@@ -13,7 +13,7 @@ from rest_framework.decorators import authentication_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date
 from django.core.mail import EmailMessage
 from django.db.models.functions import TruncDate
 from .models import RelacionUsuarioSistema
